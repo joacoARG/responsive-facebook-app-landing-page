a768dfcc8c2cd203622bee2a0bb323096e7e89a0
joacoARG <joaquincbarrera@gmail.com>
2021-09-21 20:29:32 -0300

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hello!')
+const slides = document.querySelectorAll('.slides');
+const dots = document.querySelectorAll('.dot');
+
+function setActive(i) {
+  for (slide of slides) {
+    slide.classList.remove('active');
+  }
+  slides[i].classList.add('active');
+
+  for (dot of dots) {
+    dot.classList.remove('active');
+  }
+  dots[i].classList.add('active');
+}
+
+for (let j = 0; j < dots.length; j++) {
+  dots[j].addEventListener('click', () => setActive(j));
+}
